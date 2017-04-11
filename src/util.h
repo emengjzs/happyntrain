9164afa8ca78d97a1fe1d5be6346f34529e72eee
emengjzs <emengjzs@live.com>
2017-04-11 23:24:25 +0800

    Add util.h

--- src/util.h
+++ src/util.h
@@ -4,9 +4,31 @@
 
 namespace happyntrain {
 
+// -------------------------
+// memory
+// -------------------------
 template <typename Object>
 using Ref = std::shared_ptr<Object>;
 
 template <typename Object>
 using Ptr = std::unique_ptr<Object>;
+
+// -------------------------
+// Effective C++
+// -------------------------
+
+// macro
+#define DISABLE_COPY(Clazz)     \
+ private:                       \
+  Clazz(const Clazz&) = delete; \
+  Clazz(Clazz&&) = delete;      \
+  Clazz& operator=(const Clazz&) = delete;
+
+// zero-cost class
+class NoCopy {
+  NoCopy(){};
+  DISABLE_COPY(NoCopy);
+};
+
+// --------------------------
 }
