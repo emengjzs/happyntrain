ff1a9f197fdf33df17216578bcd418275c202b0a
emengjzs <emengjzs@live.com>
2017-04-18 23:54:48 +0800

    Update DISABLE_COPY naming

--- src/util.h
+++ src/util.h
@@ -19,14 +19,15 @@ using Ptr = std::unique_ptr<Object>;
 // Effective C++
 // -------------------------
 // macro
-#define DISABLE_COPY(Clazz)     \
+#define DISABLE_COPY(Class)     \
  private:                       \
-  Clazz(const Clazz&) = delete; \
-  Clazz(Clazz&&) = delete;      \
-  Clazz& operator=(const Clazz&) = delete;
+  Class(const Class&) = delete; \
+  Class(Class&&) = delete;      \
+  Class& operator=(const Class&) = delete;
 
 // zero-cost class
 class NoCopy {
+ public:
   NoCopy(){};
   DISABLE_COPY(NoCopy);
 };
@@ -34,23 +35,23 @@ class NoCopy {
 // --------------------------
 // Fail checking
 // --------------------------
-#define EXPECT(expression, message, ...)                                       \
-  do {                                                                         \
-    if (!(expression)) {                                                       \
-      ERROR("Fail: " message, ##__VA_ARGS__);                             \
+#define EXPECT(expression, message, ...)                                     \
+  do {                                                                       \
+    if (!(expression)) {                                                     \
+      ERROR("Fail: " message, ##__VA_ARGS__);                                \
       ERROR("File: %s, Line: %d, Fail Expression: (%s)", __FILE__, __LINE__, \
-            #expression);                                                      \
-    }                                                                          \
+            #expression);                                                    \
+    }                                                                        \
   } while (0);
 
-#define EXIT_IF(expression, message, ...)                                  \
-  do {                                                                     \
-    if ((expression)) {                                                    \
-      ERROR("Fail: " message, ##__VA_ARGS__);                         \
+#define EXIT_IF(expression, message, ...)                                \
+  do {                                                                   \
+    if ((expression)) {                                                  \
+      ERROR("Fail: " message, ##__VA_ARGS__);                            \
       ERROR("File: %s, Line: %d, Unexpected Expression: (%s)", __FILE__, \
-            __LINE__, #expression);                                        \
-      exit(1);                                                             \
-    }                                                                      \
+            __LINE__, #expression);                                      \
+      exit(1);                                                           \
+    }                                                                    \
   } while (0);
 
 // end happyntrain
