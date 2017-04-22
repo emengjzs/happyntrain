0616049f570ad86c7d9e4064029d1340f32a4763
emengjzs <emengjzs@live.com>
2017-04-22 20:17:16 +0800

    Add color for error log

--- src/logging.h
+++ src/logging.h
@@ -4,11 +4,14 @@
 
 namespace happyntrain {
 
-#define LOG(message, level, ...) \
-  printf("[" level "] %s-%d " message "\n", __FILE__, __LINE__, ##__VA_ARGS__);
+#define LOG(message, level, ...)                                     \
+  printf("[%5s] %s-%d " message "\n", level, __FILENAME__, __LINE__, \
+         ##__VA_ARGS__);
+
 #define INFO(message, ...) LOG(message, "INFO", ##__VA_ARGS__);
 #define DEBUG(message, ...) LOG(message, "DEBUG", ##__VA_ARGS__);
-#define ERROR(message, ...) LOG(message, "ERROR", ##__VA_ARGS__);
+#define ERROR(message, ...) \
+  LOG("\x1b[31m" message "\x1b[0m", "ERROR", ##__VA_ARGS__);
 
 // end happyntrain
 }
