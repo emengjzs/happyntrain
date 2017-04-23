f5e868f962d823a1761b5d114a529c50277a7e81
emengjzs <emengjzs@live.com>
2017-04-23 22:50:44 +0800

    Add XXXChannel method

--- src/epoll-selector.cpp
+++ src/epoll-selector.cpp
@@ -1,13 +1,24 @@
 #include "epoll-selector.h"
 
+#if defined(__linux__)
+
 #include <unistd.h>
 #include <iostream>
+
+#include "eventloop.h"
 
 using namespace std;
 
 namespace happyntrain {
 
-EpollSelector::EpollSelector(int i) : Selector() {
+struct epoll_event EpollSelector::GetEpollEvent(Channel* channel) const {
+  struct epoll_event ev = c_struct_init<struct epoll_event>();
+  ev.events = channel->events();
+  ev.data.ptr = static_cast<void*>(channel);
+  return ev;
+}
+
+EpollSelector::EpollSelector(int i) : _id(GetNewSelectorId()) {
   DEBUG("param: %d", i);
   // FD_CLOEXEC
   _epoll_fd = epoll_create1(EPOLL_CLOEXEC);
@@ -20,11 +31,28 @@ EpollSelector::~EpollSelector() {
   INFO("epoll %d destroyed.", _epoll_fd);
 }
 
-void EpollSelector::AddChannel() {
-  cout << " add epollselector! channel" << endl;
+void EpollSelector::AddChannel(Channel* channel) {
+  DEBUG("Add Channel %lu (fd:%d) to Selector %lu", channel->id(), channel->fd(),
+        this->_id);
+  struct epoll_event event = GetEpollEvent(channel);
+  int r = epoll_ctl(_epoll_fd, EPOLL_CTL_ADD, channel->fd(), &event);
+  EXPECT(r == 0, "EPOLL_CTL_ADD Failed");
+  _activeChannels.insert(channel);
 }
 
-void EpollSelector::RemoveChannel() {}
-
-void EpollSelector::UpdateChannel() {}
+void EpollSelector::RemoveChannel(Channel* channel) {
+  DEBUG("Remove Channel %lu (fd:%d) to Selector %lu", channel->id(),
+        channel->fd(), this->_id);
+  _activeChannels.erase(channel);
 }
+
+void EpollSelector::UpdateChannel(Channel* channel) {
+  DEBUG("Update Channel %lu (fd:%d) to Selector %lu", channel->id(),
+        channel->fd(), this->_id);
+  struct epoll_event event = GetEpollEvent(channel);
+  int r = epoll_ctl(_epoll_fd, EPOLL_CTL_MOD, channel->fd(), &event);
+  EXPECT(r == 0, "EPOLL_CTL_MOD Failed");
+}
+}
+
+#endif 
