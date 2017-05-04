72599a080ff4ec1ed53f13ef6251a21a3baa0ab6
emengjzs <emengjzs@live.com>
2017-05-04 18:18:29 +0800

    Add alias now()

--- src/util/time.h
+++ src/util/time.h
@@ -10,15 +10,22 @@ namespace happyntrain {
 namespace time {
 
 using microseconds = std::chrono::microseconds;
+using milliseconds = std::chrono::milliseconds;
+using ms = std::chrono::milliseconds;
 using seconds = std::chrono::seconds;
 using minutes = std::chrono::minutes;
 using hours = std::chrono::hours;
 
 template <class T>
 int64_t current_time() {
-  std::chrono::time_point<std::chrono::steady_clock> p =
-      std::chrono::steady_clock::now();
-  return std::chrono::duration_cast<T>(p.time_since_epoch()).count();
+  using namespace std::chrono;
+  auto p = time_point_cast<ms>(steady_clock::now());
+  return duration_cast<T>(p.time_since_epoch()).count();
+}
+
+template <class T>
+inline int64_t now() {
+  return current_time<T>();
 }
 
 inline clock_t time_begin() { return std::clock(); }
