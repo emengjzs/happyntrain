f64dbed2b430056ab0ae231e6516c008055cd164
emengjzs <emengjzs@live.com>
2017-05-05 21:23:05 +0800

    Add socket func

--- src/util/net.h
+++ src/util/net.h
@@ -4,11 +4,13 @@
 #include <netinet/tcp.h>
 #include <sys/socket.h>
 
+#include <string>
+
 namespace happyntrain {
 
 namespace network {
 
-int setNonBlock(int fd, bool value = true) {
+inline int setNonBlock(int fd, bool value = true) {
   int flags = fcntl(fd, F_GETFL, 0);
   if (flags < 0) {
     return errno;
@@ -16,6 +18,15 @@ int setNonBlock(int fd, bool value = true) {
   flags = value ? flags | O_NONBLOCK : flags & ~O_NONBLOCK;
   return fcntl(fd, F_SETFL, flags);
 }
+
+inline int new_tcp_socket() { return socket(AF_INET, SOCK_STREAM, 0); }
+
+struct IP4Address {
+  std::string host;
+  short port;
+
+  IP4Address(std::string host_, short port_) : host(host_), port(port_) {}
+};
 
 // end network
 }
