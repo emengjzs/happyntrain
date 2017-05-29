52d03d077694b94499fb7592d44c31629c95f24d
emengjzs <emengjzs@live.com>
2017-05-29 20:20:56 +0800

    Update smoke test

--- main.cpp
+++ main.cpp
@@ -1,29 +1,12 @@
 #include <iostream>
-#include <set>
 #include <string>
-#include <thread>
 
 #include "src/eventloop.h"
-#include "src/selector.h"
 #include "src/tcpserver.h"
-#include "src/util/concurrent.h"
 #include "src/util/core.h"
-#include "src/util/fd.h"
-#include "src/util/net.h"
-#include "src/buffer.h"
 
 using namespace std;
 using namespace happyntrain;
-using namespace happyntrain::concurrent;
-using namespace happyntrain::network;
-using namespace happyntrain::fd;
-
-struct A {
-  int a;
-  std::string b;
-  A(int a_, std::string b_) : a(a_), b(b_) {}
-  A() : A(0, "") {}
-};
 
 int main() {
   auto eventloop = newInstance<EventLoop>(0);
@@ -31,7 +14,8 @@ int main() {
 
   server.OnConnect([](auto& socket) {
     socket->OnRead([](auto& self, auto& buffer) {
-      INFO("%s", buffer.retrieve().c_str());
+      buffer.retrieve();
+      self->Send("Hello World!");
     });
   });
 
@@ -40,9 +24,5 @@ int main() {
     INFO("Server is now listening on %d", port);
   });
   eventloop->Run();
-  // auto server = TCPServer::Create([&](auto socket) {
-  //                 socker.end("goodbye\n");
-  //               })->OnError([&](auto error) {});
-  // server.listen([&]() { DEBUG("opened server on ", server.Address()); });
   return 0;
 }
